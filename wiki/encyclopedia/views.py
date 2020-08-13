78e97801edce63a5e76f6ba655e9b470a3d59fd1
poorvikd <64829799+poorvikd@users.noreply.github.com>
2020-08-13 23:58:30 +0530

    Finished first requirement of CS50W Project_1

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
 from . import util
-
-
+from markdown2 import Markdown
+md= Markdown()
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def title(request,title):
+    page = util.get_entry(title)
+    if page!=None:
+        page=md.convert(page)
+    return render(request,"encyclopedia/search.html",context={
+        "pages":page,
+        "title":title
+    })
