0b08bc506c34e2c414509ec6fe1df4229ab15a24
poorvikd <64829799+poorvikd@users.noreply.github.com>
2020-08-15 23:14:12 +0530

    Added Create New Page functionality

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.title,name='title_search'),
-    path("search/",views.search_bar,name='search')
+    path("search/",views.search_bar,name='search'),
+    path("create/",views.create,name='create')
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from . import util
 from markdown2 import Markdown
+from django.http import HttpResponse
+from django import forms
+from django.contrib import messages
 md= Markdown()
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -35,3 +38,20 @@ def search_bar(request):
             "title": query
     })
     return index(request)
+def create(request):
+    if request.method == "POST":
+        dic=request.POST
+        title=dic['ttl']
+        entries=[i.lower() for i in util.list_entries()]
+        if title.lower() in entries:
+            messages.add_message(request, messages.ERROR, "File with same title exists")
+            return render(request, "encyclopedia/create_page.html")
+        content=dic['content']
+        util.save_entry(title=title,content=content)
+        return render(request, "encyclopedia/search.html",context={
+            "title":title,
+            "pages":content
+        })
+    return render(request, "encyclopedia/create_page.html")
+    
+    
